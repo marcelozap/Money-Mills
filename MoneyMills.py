3f184d02b23513667f8bf1016ec47e7e8a3e0beb
bloosh30 <89756422+bloosh30@users.noreply.github.com>
2021-12-08 16:45:32 -0500

    Update MoneyMills.py
    Fixed binomial distribution function was outputting extraneous solutions prior

--- MoneyMills.py
+++ MoneyMills.py
@@ -114,58 +114,71 @@ def getValue(s,keyWord):
 
 
     
-def binomialDistribution(daysToExpiration, stockPrice, bid, strikePrice ):   
-    
+def binomialDistribution(s):   
+        
     #VARIABLE SETUP
+    
+    # Number of intervals between expiriation, the more the more accurate the model
     N = 5
-
-    S0 = stockPrice
-    K = strikePrice
-    t = daysToExpiration
-
-    up = 1.05
-    down = 1/up
-    equalProbability = 0.5
-
-    r = 0.015
-
-    sxu = S0 * up
-    sxd = S0 * down
-
-    contractUp = sxu - K
-    contractDown = 0
-
-    C = numpy.exp(-r * t)(equalProbability  *contractUp + (1 - equalProbability) * contractDown)
-
-    print(sxu, sxd)
-    print(C)
-
-    S0 = float(s.last)
-    K = float(s.strike)
-
-    v = float(getValue(s,'volatility'))
-    t = float(getValue(s,'daysToExpiration'))
-
-    N = 5
-    t = t / (N - 1)     # time interval for each step
-
-
-    contractUp = numpy.exp(v * numpy.sqrt(t))
-    d = 1/u
-    p = (numpy.exp(r*t) - d)/
-
-    r = 0.015
-
-    sxu = S0 * u
-    sxd = S0 * d
-
-    C_u = sxu - K
-    C_d = 0
-
-    C = numpy.exp(-rt)(pC_u + (1-p)C_d)
-
-    print(sxu, sxd)
-    print(C)
+    
+    currentPrice = float(s.last)    #Current price of stock
+    strikePrice = float(s.strike)   #Strike for current Contract
+    
+    t = int(getValue(s,'daysToExpiration'))  #Days to expiration for contract
+    t = t / 360                              #Convert to years
+    
+    timeChange = t / (N-1)                           #Days to expiration split into the # of levels in the model
+    volatility = 0.4                                 #Volatility for current contract
+    
+    # Factor ratio for price increase or decrease
+    factorPriceIncrease = numpy.exp(volatility * numpy.sqrt(timeChange)) 
+    factorPriceDecrease = 1/factorPriceIncrease                                  
+    
+    # Current Interest Rate
+    # Probability based on irate time change and volatility
+    irate = 0.015
+    prob = (numpy.exp(irate*timeChange)-factorPriceDecrease) / (factorPriceIncrease - factorPriceDecrease)
+    
+    #print('TEST: ')
+    #print(currentPrice)
+    #print(strikePrice)
+    #print(timeChange)
+    #print(factorPriceDecrease)
+    #print(factorPriceIncrease)
+    #print(irate)
+    #print(prob)
+    
+    # Set up empty matrices
+    stockPriceMatrix = numpy.zeros((N,N))
+    callPriceMatrix = numpy.zeros((N,N))
+    
+    # Set the inital node to the current stock Price
+    stockPriceMatrix[0,0] = currentPrice
+    
+    # Fill the remaning values into the stock price matrix
+    for i in range(1, N):
+        W = i + 1
+        stockPriceMatrix[i, 0] = factorPriceDecrease * stockPriceMatrix[i-1, 0]
+        for j in range(1, W):
+            stockPriceMatrix[i, j] = factorPriceIncrease * stockPriceMatrix[i - 1, j - 1]
+            
+    # Calculating the stock price at expiration          
+    tempMatrix = stockPriceMatrix[-1,:] - strikePrice
+    
+    # Return matrix shape
+    tempMatrix.shape = (tempMatrix.size, )
+    
+    # Remove any instances of 0 because contract is worthless
+    tempMatrix = numpy.where(tempMatrix >= 0, tempMatrix, 0)
+    
+    # Set the last row to the new value check
+    callPriceMatrix[-1,:] = tempMatrix
+    
+    # Fill out the remaining call price Matrix
+    for i in range(N - 2,-1,-1):
+        for j in range(i + 1):
+            callPriceMatrix[i,j] = numpy.exp(-irate * timeChange) * ((1-prob) * callPriceMatrix[i+1,j] + prob * callPriceMatrix[i+1,j+1])
+        
 #______________________________________________________________________________________________________________________________________________________
 
 try:
